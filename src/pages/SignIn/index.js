7a09b4bddc23a46a0eb1eeb617267d3ce6f35301
Gustavo Angelo <gustavolz.angelo@gmail.com>
2020-02-13 16:38:47 -0300

    pagina-signin

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
-import Button from '~/components/Button';
-import Input from '~/components/Input';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
-
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+        </Form>
+        <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
